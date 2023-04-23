7045bd2afbb3765eecb070ff07b3c46cd440ba2a
Ekin Akyürek <akyurekekin@gmail.com>
2023-04-22 23:48:31 -0400

    update

--- utils.py
+++ utils.py
@@ -24,3 +24,22 @@ def decode(seq, vocab, rev_vocab):
     if vocab[END] in seq:
         seq = seq[:seq.index(vocab[END])+1]
     return " ".join(rev_vocab[s] for s in seq)
+
+
+from typing import Iterator, Iterable, List, Union
+import itertools
+def batch(inputs: Union[List, Iterable], n: int):
+    "Batch data into iterators of length n. The last batch may be shorter."
+    if n < 1:
+        raise ValueError("n must be at least one")
+
+    if not isinstance(inputs, Iterator):
+        inputs = iter(inputs)
+
+    while True:
+        chunk_it = itertools.islice(inputs, n)
+        try:
+            first_el = next(chunk_it)
+        except StopIteration:
+            return
+        yield itertools.chain((first_el,), chunk_it)
